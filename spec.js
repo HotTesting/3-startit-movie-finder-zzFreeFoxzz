8ce803456b0021e5d81073e7f8dec08c4899db3c
zzFreeFoxzz <freefox@bigmir.net>
2017-02-28 16:05:06 +0200

    Update spec.js

--- spec.js
+++ spec.js
@@ -1,21 +1,55 @@
 
-describe('Suite name', ()=> {
+let TestExm = function() {
+    let searchField = $("input[name='searchStr']");
+    
+    this.get = function() {
+    browser.get('https://movies-finder.firebaseapp.com/');
+  };
 
-    it('Test case name', ()=> {
-        browser.get('')
-        //search field is ElementFinder
-        let searchField = $("input[name='searchStr']")
+    this.setName = function(name) {
+    searchField.sendKeys(name);
+  };
+  
+};
 
-        let searchRequest = 'The Lord'
-        searchField.sendKeys(searchRequest)
+describe('MovieFinderSearch', ()=> {
+
+    it('positive search', function() {
+        let testExm = new TestExm;
+        let someText = 'The Lord';
+        testExm.get();
+        testExm.setName(someText);
         element(by.buttonText('Go!')).click()
+        browser.sleep(5000);
 
-        browser.sleep(5000)
+        let movieCard = $('movie-card');
+        let title = movieCard.$('h4 a').getText();
+        expect(title).toContain( someText, 'First search result should contain search string');
+    });
+   
+    it('negative search', function() {
+        let testExm2 = new TestExm;
+        testExm2.get();
+        let someText = '&&&';
+        testExm2.setName(someText);
+        element(by.buttonText('Go!')).click()
+        browser.sleep(10000);
 
-        let movieCard = $('movie-card')
-        let title = movieCard.$('h4 a').getText()
+        let movieCard = $('movie-card');
+        let title = movieCard.$('h4 a').getText();
+        expect(title).toContain(someText, 'Wrong search result');
+    })
 
-        expect(title).toContain(searchRequest, 'First search result should contain search string')
+it('searchByGenre', function() {
+        let testExm3 = new TestExm;
+        testExm3.get();
+        element(by.linkText('Fantasy')).click();
+        browser.sleep(5000);
+        $('h4 a').click();
+        browser.sleep(5000);
+        let cards = element.all(by.className('label label-info m-r-md')).getText();
+        expect(cards).toContain('Fantasy', 'Wrong search result')
+        /* Не могу придумать, как перебрать все эленты циклом и проверить каждый из них*/
     })
 
 })
