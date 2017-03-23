95f0fe087b174d1be3c4e2fa25c27b49c57f7dbd
zzFreeFoxzz <freefox@bigmir.net>
2017-03-23 16:30:54 +0200

    Update protractor.conf.js

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,5 +1,20 @@
 module.exports.config = {
-    specs: ['spec.js'],
+    specs: ['./specs/spec.js'],
     directConnect: true,
-    baseUrl: 'https://movies-finder.firebaseapp.com/'
+    baseUrl: 'https://movies-finder.firebaseapp.com/',
+    onPrepare: () => {
+        global.EC = protractor.ExpectedConditions;
+        let Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+        jasmine.getEnv().addReporter(
+            new Jasmine2HtmlReporter({
+                savePath: 'img/screenshots'
+                               }))
+    },
+    afterEach: () => {
+        browser.get('/');
+        browser.executeScript('window.sessionStorage.clear();');
+        browser.executeScript('window.localStorage.clear();');
+        browser.manage().deleteAllCookies();
+    }
+
 }
