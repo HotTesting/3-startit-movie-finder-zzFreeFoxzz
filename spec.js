2cb83f83a9cfd93189cc148cd628b8641b393819
zzFreeFoxzz <freefox@bigmir.net>
2017-03-23 16:48:19 +0200

    Update spec.js

--- spec.js
+++ spec.js
@@ -1,55 +1,33 @@
 
-let TestExm = function() {
-    let searchField = $("input[name='searchStr']");
-    
-    this.get = function() {
-    browser.get('https://movies-finder.firebaseapp.com/');
-  };
+let HomePage = require('../pageObj/HomePage.js').HomePage
 
-    this.setName = function(name) {
-    searchField.sendKeys(name);
-  };
-  
-};
+describe('Search film', ()=> {
 
-describe('MovieFinderSearch', ()=> {
+       let filmNamest = 'the lord of' 
+       beforeEach(() => {
+                     let homePage = new HomePage; 
 
-    it('positive search', function() {
-        let testExm = new TestExm;
-        let someText = 'The Lord';
-        testExm.get();
-        testExm.setName(someText);
-        element(by.buttonText('Go!')).click()
-        browser.sleep(5000);
-
-        let movieCard = $('movie-card');
-        let title = movieCard.$('h4 a').getText();
-        expect(title).toContain( someText, 'First search result should contain search string');
-    });
-   
-    it('negative search', function() {
-        let testExm2 = new TestExm;
-        testExm2.get();
-        let someText = '&&&';
-        testExm2.setName(someText);
-        element(by.buttonText('Go!')).click()
-        browser.sleep(10000);
-
-        let movieCard = $('movie-card');
-        let title = movieCard.$('h4 a').getText();
-        expect(title).toContain(someText, 'Wrong search result');
+        })   
+it('should find expected result', () => {
+        homePage.open();
+        homePage.EC(2000);
+        homePage.searchForMovie(filmName);
+        homePage.EC(3000);
+        homePage.getDisplayedResult();
+        let checkTitle = homePage.searchMovies.$$('h4 a').first().getText().then(text => text.toLowerCase())
+        expect(movieTitle).toContain(filmName, 'Search result contains wrong strings')
     })
 
-it('searchByGenre', function() {
-        let testExm3 = new TestExm;
-        testExm3.get();
-        element(by.linkText('Fantasy')).click();
-        browser.sleep(5000);
-        $('h4 a').click();
-        browser.sleep(5000);
-        let cards = element.all(by.className('label label-info m-r-md')).getText();
-        expect(cards).toContain('Fantasy', 'Wrong search result')
-        /* Не могу придумать, как перебрать все эленты циклом и проверить каждый из них*/
+ 
+    it("should't find expected result", () => {
+
+        homePage.open();
+        homePage.EC(2000);
+        homePage.searchForMovie('@@@#$%%^^&');
+        homePage.EC(3000);
+        homePage.getDisplayedResult();
+        let resultFail = homePage.searchMovies.$$('h3 + div').get(0).$$('h4 a').count()
+        expect(resultFail).toBe(0, 'Unbelievable')
     })
 
 })
